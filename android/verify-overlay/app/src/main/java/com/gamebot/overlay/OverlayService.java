a4184fbfdf70b36899726cb738aba5c75d612eed
Zhuanz <x1741877727@gmail.com>
2026-05-09 01:57:15 +0900

    build: rebuild apk (fix LinearLayout import + 文字改对掏房脚本已启动)

--- android/verify-overlay/app/src/main/java/com/gamebot/overlay/OverlayService.java
+++ android/verify-overlay/app/src/main/java/com/gamebot/overlay/OverlayService.java
@@ -15,8 +15,8 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
